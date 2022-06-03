5fbbe0894e353392a5378cce47d74daa2c37b623
chirag-08x <cagrawal081200@gmail.com>
2022-06-03 05:43:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./assets/logo.png";
+
+const App = () => {
+  return (
+    <>
+      <h1>Setting up React from Scratch </h1>
+      <img src={logo} alt="LOGO" />
+    </>
+  );
+};
+
+export default App;
